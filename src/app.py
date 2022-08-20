97854e7799f6fd0697f9c70b24a2a8f67917c16c
Alew <xalejandrow@gmail.com>
2022-08-20 18:50:22 -0300

    Ejercicio Time Series terminado (no se suben los modelos)

--- src/app.py
+++ src/app.py
@@ -1,4 +1,56 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
 
-# your code here
+from statsmodels.tsa.seasonal import seasonal_decompose
+from datetime import datetime
+from pmdarima.arima import auto_arima
+import pickle
+
+df_raw_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv')
+df_raw_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-b.csv')
+
+
+test_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv')
+test_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-b.csv')
+
+df_interim_a = df_raw_a.copy()
+df_interim_b = df_raw_b.copy()
+
+df_interim_a['datetime'] = pd.to_datetime(df_interim_a['datetime'])
+df_interim_b['datetime'] = pd.to_datetime(df_interim_b['datetime'])
+
+df_interim_a = df_interim_a.set_index(df_interim_a['datetime'])
+df_interim_b = df_interim_b.set_index(df_interim_b['datetime'])
+
+df_interim_a = df_interim_a.drop('datetime', axis=1)
+df_interim_b = df_interim_b.drop('datetime', axis=1)
+
+df_a = df_interim_a.copy()
+df_b = df_interim_b.copy()
+
+stepwise_model = auto_arima(df_a, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+print(stepwise_model.aic())
+stepwise_model.fit(df_a)
+
+filename = '../models/model_a.pkl'
+pickle.dump(stepwise_model, open(filename,'wb'))
+
+stepwise_model_b = auto_arima(df_b, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+print(stepwise_model_b.aic())
+stepwise_model_b.fit(df_b)
+filename = '../models/model_b.pkl'
+pickle.dump(stepwise_model_b, open(filename,'wb'))
